cbdc986628ebc81ffcde92e0bc6c5787c5b778c6
Dave Seidman <bullcitydave@gmail.com>
2015-02-17 21:52:34 -0500

    Works ok if steps run from command line

--- scripts/flickr.js
+++ scripts/flickr.js
@@ -1,28 +1,22 @@
 var flickrApiKey = "806745a8a5db2aff0b0cdb591b633726";
 var flickrUserId = 'toastie97';
-var flickrTag = "duffy";
-var flickrApiUrl =  "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + flickrApiKey + "&user_id=toastie97&tags=" + flickrTag +"&per_page=16&page=1&format=json&nojsoncallback=1";
+
 
 
 
 var $gallery = $('#main-gallery');
-
-$(function galleryInit() {
-  $gallery.css('display','block').flickity({
-    // options
-    cellAlign: 'center',
-    freeScroll: false,
-    contain: true,
-    wrapAround: true,
-    setGallerySize: false,
-    imagesLoaded: true,
-    percentPosition: false
-  });
-});
+// 
+// $(function galleryInit() {
+//   $gallery.flickity({
+//     // options
+//   });
+// });
 
 
 var getGallery = function (tag) {
-    $.getJSON(flickrApiUrl + "&format=json&nojsoncallback=1").done(function(photoData){
+  var flickrApiUrl =  "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + flickrApiKey + "&user_id=toastie97&tags=" + tag +"&per_page=16&page=1&format=json&nojsoncallback=1";
+
+    return $.getJSON(flickrApiUrl + "&format=json&nojsoncallback=1").done(function(photoData){
       flickrLength = Math.min(photoData.photos.photo.length,15);
       var flickrView = $('#flickr-template').html();
       var flickrImg = '';
@@ -47,11 +41,21 @@ var getGallery = function (tag) {
         $('#main-gallery').append(_.template(flickrView,({"flickrImg":flickrImg})));
         // $('#main-gallery').flickity('append' , (_.template(flickrView,({"flickrImg":flickrImg}))));
       }
-      cells = $('.gallery-cell');
-      _.each(cells, function(cell) {
-        $('#main-gallery').flickity( 'append',  cell );
-      })
-      $gallery.flickity('reloadCells');
-      return $('.gallery-cell').length;
+      // cells = $('.gallery-cell');
+      // _.each(cells, function(cell) {
+      //   $('#main-gallery').flickity( 'append',  cell );
+      // })
+    }).then(function() {
+      console.log($('.gallery-cell').length);
+      // $gallery.css('display','block').flickity({
+      //   // options
+      //   cellAlign: 'center',
+      //   freeScroll: false,
+      //   contain: true,
+      //   wrapAround: true,
+      //   setGallerySize: false,
+      //   // imagesLoaded: true,
+      //   percentPosition: false
+      // });
     });
 }
